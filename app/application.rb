366480212aba9fbf9f71dbfbe89bdbfe6c061546
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2020-07-14 13:34:47 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,48 @@
+class   Application
+
+      def call(env)
+        resp = Rack::Response.new
+         req = Rack::Request.new(env)
+
+         p @@items
+         p "testing req" ,req.path
+      if req.path =='/testing'
+       resp.status = 404
+       resp.write  "Route not found"
+     elsif req.path['items']
+
+     end
+        #  if req.path=="/songs"
+        # #   resp.write "You requested the songs"
+        # # else
+        # #   resp.write "Route not found"
+        #    p @@items
+        # end
+        #
+         resp.finish
+      # end
+    end
+end
+# class Application
+#
+#   @@items = ["Apples","Carrots","Pears"]
+#
+#   def call(env)
+#     resp = Rack::Response.new
+#     req = Rack::Request.new(env)
+#     cart = req.path.match(/cart/)
+#     if cart != nil
+#       if @@cart.empty?
+#          resp.write  "Your cart is empty\n"
+#       elsif !@@cart.empty?
+#          resp.write "#{@@cart.join("\n")}"
+#       end
+#     elsif req.path.match(/add/)
+#       if @@items.include?(req.params["item"])
+#         resp.write "added #{req.params["item"]}"
+#         @@cart << req.params["item"]
+#       else
+#         resp.write "We don't have that item"
+#       end
+#
+#     end
