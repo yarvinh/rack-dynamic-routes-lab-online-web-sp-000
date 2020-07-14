7fb3669ab74b6024a459d458e4af8df2902ec4e2
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2020-07-14 15:03:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,8 +5,6 @@ class   Application
         req = Rack::Request.new(env)
 
       items = req.path.split("/").reject {|e| e == ""}
-      #  selected_items =  @@items.select{|item| items[1] == item.name}
-
       if req.path =='/testing'
        resp.status = 404
        resp.write  "Route not found"
@@ -20,26 +18,3 @@ class   Application
 
     end
 end
-# class Application
-#
-#   @@items = ["Apples","Carrots","Pears"]
-#
-#   def call(env)
-#     resp = Rack::Response.new
-#     req = Rack::Request.new(env)
-#     cart = req.path.match(/cart/)
-#     if cart != nil
-#       if @@cart.empty?
-#          resp.write  "Your cart is empty\n"
-#       elsif !@@cart.empty?
-#          resp.write "#{@@cart.join("\n")}"
-#       end
-#     elsif req.path.match(/add/)
-#       if @@items.include?(req.params["item"])
-#         resp.write "added #{req.params["item"]}"
-#         @@cart << req.params["item"]
-#       else
-#         resp.write "We don't have that item"
-#       end
-#
-#     end
