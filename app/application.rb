0bb6d2d3dfea970712fee6a792c797d8ef6363aa
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2020-07-14 14:07:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class   Application
         req = Rack::Request.new(env)
 
       items = req.path.split("/")
-      p items.pop
+      p items.shift
       if req.path =='/testing'
        resp.status = 404
       p  req.path
