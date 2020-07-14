f963ec60f36b15f19f844403c7cbb4b93a72b288
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2020-07-14 14:25:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class   Application
        resp.write  "Route not found"
       else
         error = nil
-       @@items.each{|item| 
+       @@items.map{|item| 
          if items[1] == item.name
           resp.write   "#{item.price}"
           p resp.status
